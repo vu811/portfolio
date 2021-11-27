40d37ca4ae02e29b817c633948e8f4325795c8b0
Vu Nguyen <vunh811@gmail.com>
2021-11-27 19:25:31 +0700

    update

--- express/server.js
+++ express/server.js
@@ -10,6 +10,8 @@ app.use('/.netlify/functions/server', router) // path must route to lambda
 //app.use('/public', express.static(path.join(__dirname, '../public')))
 //app.use('/src', express.static(path.join(__dirname, '../src')))
 
+console.log(path.join(__dirname, '../public/index.html'))
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
